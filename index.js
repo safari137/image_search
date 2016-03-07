319d6a1fffbaf95075533799176acef321d9b72a
safari137 <dbdill137@gmail.com>
2016-03-07 03:14:37 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+// This is a modified version of https://github.com/vdemedes/google-images/blob/master/index.js
+
+'use strict';
+
+/**
+ * Dependencies
+ */
+
+var got = require('got');
+
+
+/**
+ * Google Images Client
+ */
+
+function Client (id, apiKey) {
+	if (!(this instanceof Client)) {
+		return new Client(id, apiKey);
+	}
+
+	this.endpoint = 'https://www.googleapis.com';
+	this.apiKey = apiKey;
+	this.id = id;
+}
+
+Client.prototype.search = function (query, options) {
+	if (!query) {
+		throw new TypeError('Expected a query');
+	}
+
+	return got(this.endpoint + '/customsearch/v1', {
+		query: this._buildOptions(query, options),
+		json: true
+	}).then(this._buildResponse);
+};
+
+Client.prototype._buildOptions = function (query, options) {
+	if (!options) {
+		options = {};
+	}
+
+	var result = {
+		q: query.replace(/\s/g, '+'),
+		searchType: 'image',
+		cx: this.id,
+		key: this.apiKey
+	};
+
+	if (options.page) {
+		result.start = options.page;
+	}
+
+	if (options.size) {
+		result.imgSize = options.size;
+	}
+
+	return result;
+};
+
+Client.prototype._buildResponse = function (res) {
+	return res.body.items.map(function (item) {
+		return {
+			url: item.link,
+			thumbnail: item.image.thumbnailLink,
+			snippet: item.snippet,
+			context: item.image.contextLink
+		};
+	});
+};
+
+
+/**
+ * Expose client
+ */
+
+module.exports = Client;
---
+++ server.js
@@ -0,0 +1,74 @@
+'use strict'
+
+var express         = require('express'),
+    googleImages    = require('./index.js'),
+    mongoose        = require('mongoose'),
+    app             = express();
+    
+app.set('view engine', 'ejs');
+
+// Setup mongoose
+var connection = mongoose.connect(process.env.MONGOLAB_URI);
+
+var historySchema = new mongoose.Schema({
+    term: String,
+    when: {type: Date, default: Date.now() }
+});
+
+var History = connection.model('History', historySchema);
+
+
+// Setup google-images
+    
+let client = googleImages('017422373164043366310:1e0qvrkc5ls', 'AIzaSyC-DynV6dTHIzHL3MfmfyBep_uc3_sWK4M');
+
+
+// Routes
+
+app.get('/api/imagesearch/:search', function(req, res) {
+    var search = req.params.search,
+        query  = req.query,
+        page   = 1;     
+        
+    addToHistory(search);
+    
+    if (query.hasOwnProperty('offset'))
+        page = query.offset;
+    
+    client.search(search, {page: page})
+        .then(function(images) {
+           res.send(images); 
+        });
+});
+
+app.get('/api/latest/imagesearch', function(req, res) {
+    History.find({}).sort('-when').limit(10).exec(function(err, history) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        res.send(history.map(function(item) {
+            return {
+                term: item.term,
+                when: item.when
+            };
+        }));
+    });
+});
+
+app.listen(process.env.PORT, function() {
+    console.log('server started...');
+});
+
+function addToHistory(item) {
+    var newItem = new History({
+        term: item
+    });
+    
+    newItem.save(function(err, savedItem) {
+        if (err) {
+            console.log(err);
+            return;
+        } 
+    });
+}
